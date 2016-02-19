6a9d70eb55413c666dddc9ecdaa9f1161afa3e61
Jarory13 <Jarory13@gmail.com>
2016-02-19 18:25:18 -0500

    Minor tweaks and adjustments to spelling.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-
+//Define the basic values for a header bio.
 var bio = {
 	"name" : "E. Jarory de Jesus",
 	"role": "Web Developer, Android Developer, Copy Writer, and Project Manager", 
@@ -10,7 +10,8 @@ var bio = {
 	"email": "Jarory13@gmail.com",
 	"phone": "813-440-9573",
 	"github": "www.github.com/Jarory13",
-	"location": "New York, NY / Tampa, FL"
+	"location": "New York, NY / Tampa, FL",
+	"linkedin": "http://bit.ly/1SXe370"
 	},
 	"biopic" : "http://bit.ly/1LxKYXH",
 	"skills" : ["Web Development", "Android", "Java", "XML", "C++", 
@@ -20,7 +21,7 @@ var bio = {
 
 }
 
-
+//pass those values into a formattted verion.
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var imageURL = HTMLbioPic.replace("%data%", bio.biopic);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -29,6 +30,7 @@ var formatedphone = HTMLmobile.replace("%data%", bio.contact.phone);
 var formattedgit = HTMLgithub.replace("%data%", bio.contact.github);
 var formattedwelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
 var formattedlocation = HTMLlocation.replace("%data%", bio.contact.location);
+var formattedlinked = HTMLlinkedin.replace("%data%", bio.contact.linkedin);
 
 
 var skillswithspace = bio.skills.join(", ")
@@ -36,15 +38,18 @@ var skillswithspace = bio.skills.join(", ")
 var formattedskills = HTMLskills.replace("%data%", skillswithspace);
 
 
+//append or prepend them to the header. 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#header").prepend(imageURL);
 $("#header").append(formattedemail);
 $("#header").append(formatedphone);
 $("#header").append(formattedgit);
+$("#header").append(formattedlinked);
 $("#header").append(formattedskills);
 $("#header").append(formattedwelcome);
 $("#header").append(formattedlocation);
+
 
 
 

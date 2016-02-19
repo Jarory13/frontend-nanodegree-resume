dd601232cea30b0560e3d83a5bedf09115aba71b
Jarory13 <Jarory13@gmail.com>
2016-02-19 18:03:41 -0500

    Updated resume builder so that the header now shows basic information like name, roles, skils, etc.. as well as a link to a picture.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,50 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Jarory");
+
+var bio = {
+	"name" : "E. Jarory de Jesus",
+	"role": "Web Developer, Android Developer, Copy Writer, and Project Manager", 
+	"contact": {
+	"email": "Jarory13@gmail.com",
+	"phone": "813-440-9573",
+	"github": "www.github.com/Jarory13",
+	"location": "New York, NY / Tampa, FL"
+	},
+	"biopic" : "http://bit.ly/1LxKYXH",
+	"skills" : ["Web Development", "Android", "Java", "XML", "C++", 
+"C", "C#", "HTML/CSS", "PHP", "JaaScript", "Unity", "Copy Writing", 
+"WordPress", "Project Management", "Excel", "Word"],
+	"welcome": "This is place holder text for a welcome message."
+
+}
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var imageURL = HTMLbioPic.replace("%data%", bio.biopic);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedemail = HTMLemail.replace("%data%", bio.contact.email);
+var formatedphone = HTMLmobile.replace("%data%", bio.contact.phone);
+var formattedgit = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedwelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+var formattedlocation = HTMLlocation.replace("%data%", bio.contact.location);
+
+
+var skillswithspace = bio.skills.join(", ")
+
+var formattedskills = HTMLskills.replace("%data%", skillswithspace);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(imageURL);
+$("#header").append(formattedemail);
+$("#header").append(formatedphone);
+$("#header").append(formattedgit);
+$("#header").append(formattedskills);
+$("#header").append(formattedwelcome);
+$("#header").append(formattedlocation);
+
+
+
+
